bca7b1df64506d2ee23c8f7dd0cb68c354284be4
Alex Rusyn <alexrusyn@users.noreply.github.com>
2025-12-25 10:39:55 +0200

    Added servo driver

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,9 +5,14 @@
 // #include "motor/motor.h"
 // #include "control/control.h"
 
+#include "esp_log.h"
+
 #include "led/led.h"
 
 #include "server/server.h"
+
+#include "servo/servo.h"
+#include "servo/servoTask.h"
 
 #define ENC_A 34
 #define ENC_B 35
@@ -15,10 +20,15 @@
 #define DIR 25
 #define EN 27
 
+static const char *TAG = "MAIN";
+
 void app_main()
 {
     initLed();
+    // initMCPWNServoChannel();
     startServer();
+
+    // xTaskCreate(servoTask, "servo_task", 2048, NULL, 5, NULL);
 
     // encoder_init(ENC_A, ENC_B);
     // motor_init(STEP, DIR, EN);
